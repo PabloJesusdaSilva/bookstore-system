46cc3df9f39866b242b46e983a5415756a914072
Pablo Jesus <91506505+PabloJesusdaSilva@users.noreply.github.com>
2024-10-07 17:45:58 -0300

    adding getters and setters in the Author class

--- Author.java
+++ Author.java
@@ -1,16 +1,36 @@
 import java.time.LocalDate;
 
 public class Author {
-    int id = 0;
+    int id;
     String name;
     LocalDate dateOfBirthDate;
 
     Author(int id, String name, LocalDate dateOfBirthDate) {
         this.id = id;
         this.name = name;
-        this.dateOfBirthDate = dateOfBirthDate;
+        this.dateOfBirthDate = new Date();
     }
 
+    public int getId() {
+        return id
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getDateOfBirthDate() {
+        return dateOfBirthDate;
+    }
+
+    public String setName(String name) {
+        this.name = name;
+    }
+
+    public Date setDateOfBirthDate(Date dateOfBirthDate) {
+        this.dateOfBirthDate = dateOfBirthDate;    
+    }  
+    
     @Override
     public String toString() {
         return "Author:" + name + "";
