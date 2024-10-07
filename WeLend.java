59082f3e9e916138e7dda92e498ba8182ed1f3b2
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2024-10-07 20:43:25 -0300

    feat: creating getters and setters We Lend class, and return book method

--- WeLend.java
+++ WeLend.java
@@ -1,10 +1,62 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 public class WeLend {
+    private int id;
+    private static int contadorId = 1;
     private Book book;
+    private String username;
+    private Date weLendDate;
+    private Date returnDate;
+    private boolean active;
 
-    WeLend(Book book) {
+
+    WeLend(Book book, String username) {
+        this.id = contadorId++;
         this.book = book;
+        this.username = username;
+        this.weLendDate = new Date();
+        this.active = true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Date getWeLendDate() {
+        return weLendDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void returnBook() {
+        this.returnDate = new Date();
+        this.active= false;
+        this.book.setAvailable(true);
+    }
+
+    @Override
+    public String toString() {
+        return "Emprestimo{" +
+                "id=" + id +
+                ", livro=" + book +
+                ", nomeUsuario='" + username + '\'' +
+                ", dataEmprestimo=" + weLendDate +
+                ", dataDevolucao=" + returnDate +
+                ", ativo=" + active +
+                '}';
     }
 }
