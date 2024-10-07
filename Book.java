98a9a24a8d253e89e9e62aa88b58a673ba21b8fa
Pablo Jesus <91506505+PabloJesusdaSilva@users.noreply.github.com>
2024-10-07 17:51:21 -0300

    adding getters and setters in the Book class

--- Book.java
+++ Book.java
@@ -1,27 +1,49 @@
 import java.time.LocalDate;
 
 public  class Book {
-    int id = 0;
-    String title;
-    Author author;
-    boolean available;
-    LocalDate registrationDate;
-    LocalDate updateDate;
+    private int id;
+    private String title;
+    private Author author;
+    private boolean available;
+    private LocalDate registrationDate;
+    private LocalDate updateDate;
 
-    Book(){}
-
-    Book(int id, String title, Author author, boolean available, LocalDate registrationDate, LocalDate updateDate) {
+    Book(int id, String title, Author author) {
         this.id = id;
         this.title = title;
         this.author = author;
-        this.available = available;
-        this.registrationDate = registrationDate;
-        this.updateDate = updateDate;
+        this.available = true;
+        this.registrationDate = new Date();
+        this.updateDate = new Date();
     }
 
     public int getId() {
         return id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public Date registrationDate() {
+        return registrationDate;
+    }
+
+    public Date updateDate() {
+        return updateDate;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        this.updateDate = new Date();
 
     public void setAvailable(boolean available) {
         this.available = available;
