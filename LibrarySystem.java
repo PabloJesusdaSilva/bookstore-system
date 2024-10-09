ea3540ed0003a92a41bed36b2858b38f0b31cd92
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2024-10-08 21:32:09 -0300

    :refactor: refactoring the LibrarySystem class

--- LibrarySystem.java
+++ LibrarySystem.java
@@ -3,11 +3,35 @@ import java.util.Date;
 import java.util.List;
 
 public class LibrarySystem {
-    
+    public static final String WELCOME_MESSAGE = "Do you want to borrow a book? (yes/no)";
+    public static final String TANK_YOU_MESSAGE = "Thank you for using the library system.";
+    public static final String INVALID_RESPONSE_MESSAGE = "Invalid response. Please respond with 'yes' or 'no'.";
+
     public static void main(String[] args) {    
         Bookstore bookstore = new Bookstore();
         Scanner scanner = new Scanner(System.in);
 
+        setupLibrary(bookstore);
+
+        while(true) {
+            System.out.println(WELCOME_MESSAGE);
+            String response = scanner.nextLine().trim().toLowerCase();
+
+            switch (response) {
+                case "yes":
+                    handleBookBorrowing(bookstore, scanner);
+                    break;
+                case "no":
+                    System.out.println(TANK_YOU_MESSAGE);
+                    return;
+                default:
+                    System.out.println(INVALID_RESPONSE_MESSAGE);
+                    break;
+            }
+        }
+    }
+
+    private static void setupLibrary(Bookstore bookstore) {
         Author author1 = new Author(1, "J.K. Rowling", new Date());
         Author author2 = new Author(2, "J.R.R. Tolkien", new Date());
 
@@ -21,46 +45,41 @@ public class LibrarySystem {
         bookstore.addBook(book1);
         bookstore.addBook(book2);
         bookstore.addBook(book3);
+    }
 
-        while(true) {
-            System.out.println("Do you want to borrow a book? (yes/no)");
-            String response = scanner.nextLine().toLowerCase();
+    private static void handleBookBorrowing(Bookstore bookstore, Scanner scanner) {
+        List<Book> availableBooks = bookstore.listAvailableBooks();
 
-            if(response.equals("yes")) {
-                List<Book> availableBooks = bookstore.listAvailableBooks();
-
-                if(availableBooks.isEmpty()) {
-                    System.out.println("Not have available books");
-                } else {
-                    System.out.println("Available books:");
-
-                    for(Book book: availableBooks) {
-                        System.out.println(book.getId() + ": " + book.getTitle());
-                    }
-
-                    System.out.println("Enter the ID of the book you wish to borrow: ");
-                    int idBook = scanner.nextInt();
-                    scanner.nextLine();
-
-                    Book selectedBook = bookstore.getBookForId(idBook);
-
-                    if(selectedBook != null && selectedBook.isAvailable()) {
-                        System.out.println("Enter your name: ");
-                        String username = scanner.nextLine();
-
-                        bookstore.lendBook(selectedBook, username);
-                        System.out.println("The book " + selectedBook.getTitle() + " was loaned to " + username);
-                    } else {
-                        System.out.println("Book not found or not available for loan.");
-                    }
-                }              
-            } else if(response.equals("no")) {
-                System.out.println("Thank you for using the library system.");
-            } else {
-                System.out.println("Invalid response. Please respond with 'yes' or 'no'.");
-            }
-
+        if(availableBooks.isEmpty()) {
+            System.out.println("Not have available books");
+        } else {
+            displayAvailableBooks(availableBooks);
+            borrowBook(bookstore, scanner);
         }
-    
+    }
+
+    private static void displayAvailableBooks(List<Book> availableBooks) {
+        System.out.println("Available books:");
+
+        for(Book book: availableBooks) {
+            System.out.println(book.getId() + ": " + book.getTitle());
+        }
+    }
+
+    private static void borrowBook(Bookstore bookstore, Scanner scanner) {
+        System.out.println("Enter the ID of the book you wish to borrow: ");
+        int idBook = Integer.parseInt(scanner.nextLine());
+        Book selectedBook = bookstore.getBookForId(idBook);
+
+        if(selectedBook != null && selectedBook.isAvailable()) {
+            System.out.println("Enter your username: ");
+            String username = scanner.nextLine();
+
+            bookstore.lendBook(selectedBook, username);
+            
+            System.out.println("The book " + selectedBook.getTitle() + " was loaned to " + username);
+        } else {
+            System.out.println("Book not found or not available for loan.");
+        }
     }
 }
